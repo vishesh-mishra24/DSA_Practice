358efc3fc3bfdbc2f5b719bd0a2f594a5e52a896
vishesh-mishra24 <130219037+vishesh-mishra24@users.noreply.github.com>
2025-10-09 23:51:19 +0530

    Create MinSizeSubarraySum.java
    code for minsize subarraysum

---
+++ Sliding Window/MinSizeSubarraySum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n=nums.length;
+        int i=0,j=0;
+        int sum=0;
+        int minL=n + 1;
+        while(j<n){
+            sum+=nums[j];
+            while(sum>=target){
+                minL=Math.min(minL,j-i+1);
+                sum-=nums[i];
+                i++;
+            }
+            j++;
+        }
+        return minL==n+1 ?0:minL;
+    }
+}
