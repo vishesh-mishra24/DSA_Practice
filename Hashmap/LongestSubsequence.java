6b4f0dc68e85ed8fe952f2a60ac6c9fc896f966e
vishesh-mishra24 <130219037+vishesh-mishra24@users.noreply.github.com>
2025-11-09 16:42:10 +0530

    Create LongestSubsequence.java

---
+++ Hashmap/LongestSubsequence.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            // Only start counting if num is the beginning of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Count forward
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longest = Math.max(longest, currentStreak);
+            }
+        }
+
+        return longest;
+    }
+}
