eb38c4decaa16fc426fa284738b3cb8bcf4fe341
vishesh-mishra24 <130219037+vishesh-mishra24@users.noreply.github.com>
2025-12-12 21:13:32 +0530

    Create BinaryTreeRightSideView.java

---
+++ Tree/BinaryTreeRightSideView.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, 0, result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, int level, List<Integer> result) {
+        if (node == null) return;
+
+        // If this is the first time reaching this level
+        if (level == result.size()) {
+            result.add(node.val);
+        }
+
+        // Go right first, then left
+        dfs(node.right, level + 1, result);
+        dfs(node.left, level + 1, result);
+    }
+}
